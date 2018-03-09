f0d88a19e675dd3149edb19b82b677430df512e8
anhhnt <ntahoang@gmail.com>
2018-03-09 20:02:09 +0700

    Small changes

--- react/src/Home.js
+++ react/src/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import Paper from 'material-ui/Paper';
@@ -12,11 +13,21 @@ const paperStyle = {
   lineHeight: '50px',
 };
 
+const RoutingButton = withRouter(({ history }) => (
+  <RaisedButton label="Access" primary={true} onClick={() => {
+    history.push(`/event/${1234}`)
+  }} />
+));
+
 export default ({ login, loginChange, eventCodeSubmit, logInAsAdmin }) => (
   <div>
-    <TextField floatingLabelText="Enter event code" value={login.evenCode} onChange={loginChange('eventCode')} />
+    <TextField
+      floatingLabelText="Enter event code"
+      value={login.evenCode}
+      onChange={loginChange('eventCode')}
+    />
     <br />
-    <RaisedButton label="Access" primary={true} onClick={eventCodeSubmit}/>
+    <RoutingButton/>
     <br />
     <Paper style={paperStyle} zDepth={1} circle={true}>
       Or
@@ -24,11 +35,22 @@ export default ({ login, loginChange, eventCodeSubmit, logInAsAdmin }) => (
     <br />
     Log in as event manager
     <br />
-    <TextField type="email" floatingLabelText="Email" value={login.email} onChange={loginChange('email')}/>
+    <TextField
+      type="email"
+      floatingLabelText="Email"
+      value={login.email}
+      onChange={loginChange('email')}
+    />
     <br />
-    <TextField hintText="Password Field" floatingLabelText="Password" value={login.password} onChange={loginChange('password')} type="password" />
+    <TextField
+      hintText="Password Field"
+      floatingLabelText="Password"
+      value={login.password}
+      onChange={loginChange('password')}
+      type="password"
+    />
     <br />
-    <RaisedButton label="Login" primary={true} onClick={logInAsAdmin}/>
+    <RaisedButton label="Login" primary={true} onClick={logInAsAdmin} />
     <br />
     <br />
     <RaisedButton label="Signup and login" primary={true} />
