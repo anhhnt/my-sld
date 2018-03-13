2b141eb67b605cf44a67835e27c399b7783c74a1
anhhnt <ntahoang@gmail.com>
2018-03-13 10:29:20 +0700

    Fixed static files serving

--- src/app.js
+++ src/app.js
@@ -29,11 +29,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
-//const publicPath = app.get('react/public');
-//console.log(publicPath);
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, '../react/build')));
 
 // Set up Plugins and providers
+app.configure(express.rest());
 app.configure(socketio({
   path: '/event/'
 }, function(io) {
